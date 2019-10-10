3a06aeb20b6324b68b89ea8a6478c39b2ab14ed9
Alyssa <astefenc@asu.edu>
2019-10-09 23:49:27 -0700

    read the amount of years
    getting standard input and converting to integer, started to get year

---
+++ storm.cpp
@@ -0,0 +1,23 @@
+//Alyssa Stefencavage
+#include <string>
+#include <iostream>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    string input;
+    cin >> input;//reading in the line
+    
+    int numberOfYears = input[0] - '0'; //converting first to amount of years
+
+    int i = 2;
+    while(i<input.length())
+    {
+        
+    }
+    
+
+}
