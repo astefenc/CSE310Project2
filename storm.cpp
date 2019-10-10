d4683a4d4b7e838c1534a149dee9cf08ae37ebec
Alyssa <astefenc@asu.edu>
2019-10-10 10:49:39 -0700

    file length
    opening details-YYYY.csv for each year and getting the length

--- storm.cpp
+++ storm.cpp
@@ -4,20 +4,48 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
+#include "defn.h"
+#include <fstream>
 using namespace std;
 
 int main()
 {
     string input;
     cin >> input;//reading in the line
-    
     int numberOfYears = input[0] - '0'; //converting first to amount of years
-
-    int i = 2;
-    while(i<input.length())
+   
+    int yearCount = 0;
+    string givenYear[numberOfYears]; //get the years into integers
+    while(yearCount<numberOfYears) //grab the year values
     {
-        
+        cin>>givenYear[yearCount];
+        yearCount++;
     }
+
+    storm_event stormEventArrays[numberOfYears]; //create amount of arrays for input given
+    int fileLength[numberOfYears];//get the length of each file
+    yearCount = 0; //make count 0 again
+    while(yearCount<numberOfYears) //while the count is less
+    {
+        string fileName = "details-"+givenYear[yearCount]+".csv"; //set the file name equal to that year
+        ifstream fin(fileName);//open file 
+        string fileInput; //get input
+        fileLength[yearCount] = 0; //set the file length equal to zero
+        getline(fin,fileInput); //skip the first line
+        while(getline(fin,fileInput)) //read the input
+        {
+            fileLength[yearCount]++; //increment the length
+        }
+        yearCount++; //go to next year
+    }
+    //storm_event stormEventArrays[numberOfYears]; //create amount of arrays for input given
+
+    
+    
+    
+    
+    
+    
     
 
 }
